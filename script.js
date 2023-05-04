e374da8abcec37f97ec8be8a32a6c3cdc80238fc
riazaia <42520601+riazaia@users.noreply.github.com>
2023-05-04 18:35:40 -0300

    Refactor code for clarity

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const container = document.querySelector("div.container");
-let gridSize = "16";
+let gridSize = 16;
+let pencilColor = "hsla(0, 0%, 20%, 1)";
 
-container.style.cssText = `grid-template-columns: repeat(${+gridSize}, 1fr);`;
+container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`;
 
-for (let i = 1; i <= +gridSize * +gridSize; i++) {
+for (let i = 1; i <= gridSize ** 2; i++) {
   let currentDiv = document.createElement(`div`);
-  currentDiv.classList.add(`${i}`);
+  currentDiv.setAttribute("data-id", `${i}`);
   container.appendChild(currentDiv);
-}
+} 
 
 const allDivs = container.querySelectorAll(`div`);
 
 allDivs.forEach((div) =>
   div.addEventListener("mouseleave", (e) => {
-    div.style.backgroundColor = "#333";
+    div.style.backgroundColor = pencilColor;
   })
 );
 
